e450ae34d49bd9c584548a0564ed928c0587f20a
Craig Clayton <craig__clayton@outlook.com>
2024-03-16 22:02:14 +0000

    make Product component

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className='bg-gray-900 text-white h-16 text-center flex border-b-2 border-red-600'>
+      <div className='flex items-center'>
+        <h3 className=''>Navbar</h3>
+      </div>
+    </nav>
+  );
+}
---
+++ src/app/components/Product.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Product({ product }) {
+  return (
+    <div className='text-center'>
+      <h2>{product.title}</h2>
+      <div>
+        <Image
+          src={product.images[1]}
+          width={150}
+          height={150}
+          className='m-auto'
+        />
+        <Link href='/'>
+          <button></button>
+        </Link>
+      </div>
+    </div>
+  );
+}
